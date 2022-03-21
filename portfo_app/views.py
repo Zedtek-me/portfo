5f1db86f8e6a38af7209fe2bd211a976b9d18b4a
unknown <zechariahadebayo42@gmail.com>
2022-03-21 09:35:31 +0100

    'changes'

--- portfo_app/views.py
+++ portfo_app/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', {})
-
-
-def thanks(req):
-    if req.method == 'POST':
-        req.post.get()
+    if request.method == "POST":
+        print(request.POST)
+        name= request.POST.get('name')
+        email= request.POST.get('Email')
+        num= request.POST.get('tel')
+        mgs= request.POST.get('message')
+        messages.success(request, 'your information has been sent. thnaks!')
+        return redirect('')
     else:
-        pass
-    return render(req, 'thanks.html',{})
+        msg=  messages.get_messages(request)
+        return render(request, 'index.html', {'msg': msg})
+
