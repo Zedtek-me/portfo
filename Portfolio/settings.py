ae97d93dfbca3096470f67d7e1a870aa94d873a2
unknown <zechariahadebayo42@gmail.com>
2022-03-24 09:24:34 +0100

    updated things

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0lidss)@6@*w4h4%#qv=u^%g_gcxov-@l$+fv=q+fig0jsvv4d'
-
+SECRET_KEY = os.environ.get('PORTFO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
